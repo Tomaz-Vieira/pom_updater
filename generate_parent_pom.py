a91d8c09dc2bfe931bb58155377bf856a5b4253d
Tomaz Vieira <tom.fogaca@gmail.com>
2019-06-06 14:33:59 +0200

    Cleans up a bit, adds bump parent version

--- generate_parent_pom.py
+++ generate_parent_pom.py
@@ -1,9 +1,13 @@
 import subprocess
 import xml.dom.minidom as minidom
+import os
 import sys
 
 def findChildren(parentNode, tagName):
     return [nd for nd in parentNode.childNodes if hasattr(nd, 'tagName') and nd.tagName == tagName]
+
+def findChild(parentNode, tagName):
+    return findChildren(parentNode, tagName)[0]
 
 def findChildrenBySuffix(parentNode, suffix):
     return [nd for nd in parentNode.childNodes if hasattr(nd, 'tagName') and nd.tagName.endswith(suffix)]
@@ -14,12 +18,39 @@ def findDependencyVersion(depName:str, depDir:str):
 def getChildValue(node, childTagName:str):
     return findChildren(node, childTagName)[0].firstChild.nodeValue
 
+def getNodeValue(node):
+    return node.firstChild.nodeValue
+
+def setNodeValue(node, value):
+    node.firstChild.nodeValue = value
+
 class Pom:
+    @classmethod
+    def getPomsFromDir(cls, projectsDir:str):
+        pomPaths = subprocess.check_output(['find', projectsDir, '-iname', 'pom.xml'], universal_newlines=True).splitlines()
+        return [cls(path) for path in pomPaths]
+
     def __init__(self, path:str):
+        self.path = path
         self.xml = minidom.parse(path)
-        self.projectNode = findChildren(self.xml, 'project')[0]
-        self.artifactId = getChildValue(self.projectNode, 'artifactId')
-        self.version = getChildValue(self.projectNode, 'version')
+        self.projectNode = findChild(self.xml, 'project')
+        self.versionNode = findChild(self.projectNode, 'version')
+        self.parentNode = findChild(self.projectNode, 'parent')
+        self.parentVersionNode = findChild(self.parentNode, 'version')
+
+    def __eq__(self, other):
+        return os.path.abspath(self.path) == os.path.abspath(other.path)
+
+    @property
+    def artifactId(self):
+        return getChildValue(self.projectNode, 'artifactId')
+
+    def update_original(self):
+        pass
+
+    @property
+    def version(self):
+        return getNodeValue(self.versionNode)
 
     def getPropertiesNodes(self):
         return findChildren(self.projectNode, 'properties')
@@ -37,8 +68,7 @@ class Pom:
         return f"<Pom {self.artifactId} {self.version}>"
 
     def updateDependencyVersions(self, projectsDir:str):
-        pomPaths = subprocess.check_output(['find', projectsDir, '-iname', 'pom.xml'], universal_newlines=True).splitlines()
-        poms = [Pom(path) for path in pomPaths]
+        poms = self.getPomsFromDir(projectsDir)
         pom_dict = {p.artifactId:p for p in poms}
         for depNode in self.getDependencyNodes():
             depName = depNode.tagName.replace('.version', '')
@@ -46,16 +76,29 @@ class Pom:
                 depNode.firstChild.nodeValue = pom_dict[depName].version
             else:
                 print(f"WARNING: Could not find pom for dependency '{depName}' in directory '{projectsDir}'", file=sys.stderr)
+        self.bumpVersion()
 
-        return self.xml.toprettyxml(encoding='UTF-8')
+    def bumpVersion(self):
+        print(f"this is version::::::::::::::::: {self.version}")
+        version_components = [int(comp) for comp in self.version.split('.')]
+        version_components[-1] += 1
+        setNodeValue(self.versionNode, '.'.join((str(comp) for comp in version_components)))
+
+    def updateParentVersion(self, parentVersion):
+        setNodeValue(self.parentVersionNode, parentVersion)
+
+    def __str__(self):
+        lines = []
+        for line in self.xml.toprettyxml(encoding='UTF-8').decode('utf-8').splitlines():
+            if line.strip() != '':
+                lines.append(line)
+        return '\n'.join(lines)
 
 
 if __name__ == '__main__':
     pom = Pom(sys.argv[1])
-    for line in pom.updateDependencyVersions(sys.argv[2]).decode('utf-8').splitlines():
-        if line.strip() == '':
-            continue
-        print(line)
+    pom.updateDependencyVersions(sys.argv[2])
+    print(pom)
 
 
 
